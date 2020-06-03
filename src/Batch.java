05d6d5c84e0d1b413dbc4ee92d07d3ee8bb03e23
CharlieMuir19 <charliemuir2141@gmail.com>
2020-06-03 12:55:18 +0100

    implementation of farm code, batch weight and confirmation

--- src/Batch.java
+++ src/Batch.java
@@ -1,4 +1,9 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
@@ -6,18 +11,17 @@ import java.util.Scanner;
 public class Batch {
 
     private String date;
-    private int farmCode;
+    private String farmCode;
     private String finalFruit;
     private String fruitCode;
+    private int batchWeight;
+    private String batchNumber;
 
     //begins gathering information from user
     public Batch() {
         printWelcome();
         startChoice();
         generateDate();
-        askFarmCode();
-        askFruitType();
-
     }
 
     private void printWelcome() {
@@ -32,6 +36,9 @@ public class Batch {
         int createBatch = 1;
         int quit = 2;
         int startChoice = Parser.decide(createBatch, quit);
+        if (startChoice == 2){
+            System.exit(0);
+        }
     }
 
     //The date is generated and stored to constructor
@@ -46,6 +53,7 @@ public class Batch {
     public void displayDate(String date) {
         System.out.println("Date: " + date);
         this.date = date;
+        askFarmCode();
     }
 
     public void askFarmCode() {
@@ -53,12 +61,14 @@ public class Batch {
         System.out.print("\n >");
         int minFarmCode = 0;
         int maxFarmCode = 999;
-        farmCode = Parser.decide(minFarmCode, maxFarmCode);
+        int temp = Parser.decide(minFarmCode, maxFarmCode);
+        farmCode = String.format("%03d", temp);
         printFarmCode();
     }
 
     private void printFarmCode() {
         System.out.println("Farm Code: " + farmCode);
+        askFruitType();
     }
 
     private void askFruitType() {
@@ -89,26 +99,77 @@ public class Batch {
 
     private void askBatchWeight() {
         System.out.println("\nEnter Batch Weight in KGs(max weight per batch is 100kg)" + "\n >");
-        int choice = Parser.decide(1, 100);
-
-
+        batchWeight = Parser.decide(1, 100);
+        System.out.println("Batch Weight :" + batchWeight);
+        validateBatch();
     }
 
+    private void validateBatch() {
+        System.out.println("This batch contains " + batchWeight + " kgs of " +  finalFruit +
+                " \nfrom Farm Number " + farmCode + " recieved on " + date + ". Is this correct?");
+        System.out.println("\n1. YES");
+        System.out.println("\n2. NO");
+        int correctBatch = Parser.decide(1,2);
+        if (correctBatch == 2){
+            generateDate();
+        } else if (correctBatch == 1){
+            askSave();
+        }
+    }
+
+    private void askSave() {
+        System.out.println("Would you like to save these details?");
+        System.out.println("\n1. YES");
+        System.out.println("\n2. NO - FINISH");
+        int correctBatch = Parser.decide(1,2);
+        if (correctBatch == 2){
+            new Batch();
+        } else if (correctBatch == 1){
+            generateBatchNumber();
+        }
+    }
+
+    private void generateBatchNumber() {
+        batchNumber = date + "-" + fruitCode + "-" + farmCode;
+        //saveDataFile(batchNumber);
+    }
+
+//    private void saveDataFile(String filename) {
+//        try {
+//            Path filepath = Paths.get("C:\\Users\\GA\\Documents\\Year1\\CS112 Programming 1- T3 Project\\step1Output\\" + batchNumber + ".txt");
+//            File outputFile = filepath.toFile();
+//            if (!outputFile.exists()) {
+//                outputFile.createNewFile();
+//            }
+//            FileWriter writer = new FileWriter(filepath.toString());
+//            writer.write(batchDetails());
+//            writer.close();
+//            askPrintDetails();
+//        } catch (IOException ioe) {
+//            dataDisplay.append("There was a problem writing to " + filename);
+//            dataDisplay.append("Error message: " + ioe.getMessage());
+//        }
+//
+//    }
 
     private void setSTFruit() {
         finalFruit = "Strawberry";
         fruitCode = "ST";
+        System.out.println("You choose: " + finalFruit);
     }
     private void setRAFruit() {
         finalFruit = "Raspberry";
         fruitCode = "RA";
+        System.out.println("You choose: " + finalFruit);
     }
     private void setBLFruit() {
         finalFruit = "Blackberry";
         fruitCode = "BL";
+        System.out.println("You choose: " + finalFruit);
     }
     private void setGOFruit(){
         finalFruit = "Gooseberry";
         fruitCode = "GO";
+        System.out.println("You choose: " + finalFruit);
     }
 }
--- src/Parser.java
+++ src/Parser.java
@@ -11,24 +11,22 @@ public class Parser {
 
     public static int decide(int minChoice, int maxChoice) {
         int choice = 0;
-
+        boolean valid = false;
         if (reader.hasNextInt()) {
             choice = reader.nextInt();
-            //String tempChoice = Integer.toString(choice);
-            //while(!reader.hasNextInt()){
             if (choice < minChoice) {
                 System.out.println("No valid input - too low");
                 reader.nextInt();
             } else if (choice > maxChoice) {
                 System.out.println("No valid input - too high");
                 reader.nextInt();
-//            } else if (tempChoice.matches(".*[a-z].*"))
-//                System.out.println("Error - cannot enter characters, enter a valid number ");
-//            reader.nextInt();
-            }
-        }return choice;
+            } else if (choice >= minChoice && choice <= maxChoice)
+                valid = true;
+        }
+        return choice;
     }
 }
+
 
 
 
